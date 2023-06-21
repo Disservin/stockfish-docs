0e61b3a6a0fe40587076715fac8a6511555ddb07
disservin <disservin.social@gmail.com>
2023-06-21 20:19:46 +0200

    ci

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
 	title: "Stockfish",
 	description: "Stockfish documentation",
+	base: "/stockfish-docs/",
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		nav: [
