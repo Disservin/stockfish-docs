a4063604058e1fcf119ff13bb3b44ee551f3d4d4
disservin <disservin.social@gmail.com>
2023-06-21 20:15:22 +0200

    Initial commit

---
+++ .vitepress/config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+	title: "Stockfish",
+	description: "Stockfish documentation",
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: "Home", link: "/" },
+			{ text: "Docs", link: "/pages/home" },
+		],
+
+		sidebar: [
+			{
+				text: "Home",
+				items: [
+					{ text: "Description", link: "/pages/home" },
+					{ text: "Advanced topics", link: "/pages/advanced-topics" },
+					{ text: "Commands", link: "/pages/commands" },
+					{
+						text: "Compiling from source",
+						link: "/pages/compiling-from-source",
+					},
+					{ text: "Developers", link: "/pages/developers" },
+					{ text: "Download and Usage", link: "/pages/download-and-usage" },
+					{
+						text: "Governance and responsibilities",
+						link: "/pages/governance-and-responsibilities",
+					},
+					{ text: "Regression Tests", link: "/pages/regression-tests" },
+					{ text: "Stockfish FAQ", link: "/pages/stockfish-faq" },
+					{ text: "Terminology", link: "/pages/terminology" },
+					{ text: "Useful data", link: "/pages/useful-data" },
+				],
+			},
+		],
+
+		socialLinks: [
+			{
+				icon: "github",
+				link: "https://github.com/official-stockfish/Stockfish",
+			},
+		],
+	},
+	vite: {},
+});
---
+++ index.md
@@ -0,0 +1,20 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Stockfish"
+  text: "An open source chess engine"
+  tagline: Documentation, Technical Details and FAQ
+  actions:
+    - theme: brand
+      text: FAQ
+      link: /pages/stockfish-faq
+features:
+  - title: Latest Dev Builds
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Latest Release
+    details: Stockfish 15.1
+  - title: Legal dispute
+    details: ChessBase GmbH and the Stockfish team reach an agreement and end their legal dispute
+---
