e72e37914ab394ed77f6c185dfe68ffd5762263d
Disservin <disservin.social@gmail.com>
2024-01-12 21:04:22 +0100

    add auto sidebar

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitepress";
+import { getSidebar } from 'vitepress-plugin-auto-sidebar'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -23,77 +24,12 @@ export default defineConfig({
 			{ text: "Docs", link: "/pages/Home" },
 		],
 
-		sidebar: [
-			{
-				text: "Stockfish",
-				items: [
-					{ text: "Description", link: "/pages/Home" },
-					{ text: "Advanced topics", link: "/pages/Advanced-topics" },
-					{ text: "Commands", link: "/pages/Commands" },
-					{
-						text: "Compiling from source",
-						link: "/pages/Compiling-from-source",
-					},
-					{ text: "Developers", link: "/pages/Developers" },
-					{
-						text: "Download and Usage",
-						link: "/pages/Download-and-usage",
-					},
-					{
-						text: "Governance and responsibilities",
-						link: "/pages/Governance-and-responsibilities",
-					},
-					{
-						text: "Regression Tests",
-						link: "/pages/Regression-Tests",
-					},
-					{ text: "Stockfish FAQ", link: "/pages/Stockfish-FAQ" },
-					{ text: "Terminology", link: "/pages/Terminology" },
-					{ text: "Useful data", link: "/pages/Useful-data" },
-				],
-			},
-			{
-				text: "NNUE Pytorch",
-				items: [
-					{ text: "Documentation", link: "/pages-nnue/docs/nnue" },
-					{ text: "Features", link: "/pages-nnue/docs/features" },
-				],
-			},
-
-			{
-				text: "Fishtest",
-				items: [
-					{
-						text: "Creating a Test",
-						link: "/pages-fishtest/Creating-my-first-test",
-					},
-					{
-						text: "Running the worker",
-						link: "/pages-fishtest/Running-the-worker",
-					},
-					{
-						text: "Fishtest Server Setup",
-						link: "/pages-fishtest/Fishtest-server-setup",
-					},
-					{
-						text: "Advanced Topics",
-						link: "/pages-fishtest/Advanced-topics",
-					},
-					{
-						text: "Build Cutechess With Qt5 Static",
-						link: "/pages-fishtest/Build-cutechess-with-Qt5-static",
-					},
-					{
-						text: "Fishtest Mathematics",
-						link: "/pages-fishtest/Fishtest-mathematics",
-					},
-					{
-						text: "PGN files of games played on fishtest",
-						link: "/pages-fishtest/PGN-files-of-games-played-on-fishtest",
-					},
-				],
-			},
-		],
+    sidebar: getSidebar({ 
+      contentRoot: '/',
+      contentDirs: ['pages', 'pages-fishtest', 'pages-nnue'],
+      collapsible: true,
+      collapsed: false 
+    }),
 
 		socialLinks: [
 			{
